a8886886afcee0add41be8383b028b893ffa0812
Bones <terraw1999@proton.me>
2026-03-22 01:23:36 -0600

    Add: Enemy pathfinding logic

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
+const SPEED = 80.0
+
+
+var direction := 1
 
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSlime
+@onready var ray_cast_down: RayCast2D = $AnimatedSlime/RayCastDown
 
 
 func _physics_process(delta: float) -> void:
@@ -14,7 +18,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := choose_direction()
+	direction = choose_direction(direction)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -25,14 +29,28 @@ func _physics_process(delta: float) -> void:
 	# TODO: If damaged play damage animation and then vanish, else default
 	
 	if direction > 0:
-		sprite.flip_h = false
+		sprite.scale.x = 1
 	elif direction < 0:
-		sprite.flip_h = true
+		sprite.scale.x = -1
 
 	move_and_slide()
 
 
 # Enemy AI
-func choose_direction() -> float:
-	return 1
-	
+func choose_direction(direction) -> int:
+	if is_on_wall():
+		print("Slime is on wall!")
+		if direction > 0:
+			return -1
+		else:
+			return 1 
+
+
+	if !ray_cast_down.is_colliding():
+		print("Slime is on edge!")
+		if direction > 0:
+			return -1
+		else:
+			return 1
+	else:
+		return direction
